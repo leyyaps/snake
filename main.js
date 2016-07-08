a36b90792299245dec1a85392ffa36483bda2009
leyyaps <andrew.smith@ipsofactouk.com>
2016-07-08 17:36:19 +0100

    working snake with working random food. Death with walls, not with self

--- main.js
+++ main.js
@@ -4,15 +4,18 @@ var snake=["1_8","2_8","3_8","4_8"]; //intial snake array
 var direction = "down"; // direction starts down
 var food=""; // no food till randomFoodGenerator
 var speed = 200;
+var timerId;
 // Creating the cells in the board
-// (1) Board is 600 x 400 so can take 30 cells wide and 20 blocks tall. They are snake-cells.
-// (2) Need to append the game-box with screen cells. 
-// (3) Let r = row and c=column. Then a number to correspond to where it is - e.g. r_10. -->  cell_row_column
+// Board is 600 x 400 so can take 30 cells wide and 20 blocks tall. They are snake-cells.
+// Need to append the game-box with screen cells. 
+// Let r = row and c=column. Then a number to correspond to where it is - e.g. r_10. -->  cell_row_column
 function startGame() {
 //  Snake isn't really moving(ha! I know - obvious but just to remind myself) its having its tail chopped of and then head extended to give the appearance of movement.Snake can therefore be represented by using an array. We then can push and pop the cells from that array to make it move and stick an extra cell on when it eats
   direction = "down";
   snake = ["1_8","2_8","3_8","4_8"];
   food="";
+
+  console.log("startGame");
  
   $('#game-box').html(""); // Get the box 
  //Create the cells within the game-box that later become snake cells as the snake grows 
@@ -26,8 +29,8 @@ function startGame() {
       $('#cell_2_8').addClass('snake-cell');
       $('#cell_3_8').addClass('snake-cell');
       $('#cell_4_8').addClass('snake-cell');
-      randomFoodGenerator();
-      setTimeout(function(){snakeMove()}, speed);
+      // randomFoodGenerator();
+      timerId = setInterval(snakeMove, speed);
 }
 
 startGame();
@@ -38,10 +41,14 @@ function randomFoodGenerator(){
   $('#cell_'+r1+'_'+c1).addClass('food');
      food=''+r1+'_'+c1;
 };
-// randomFoodGenerator();
+randomFoodGenerator();
 // console.log(food);
 
 function snakeMove() {
+
+
+
+
 
   // Removing the tail of the snake
   var tail=snake.pop(); 
@@ -64,7 +71,7 @@ function snakeMove() {
       case "right": c=c+1; break;  // Right
       case "down": r=r+1; break; // Bottom
     }  
-  var eat=""+r+"_"+c;
+  var eat = r + "_" + c;
   // So now if the eat cell is equal to the food square we know to add to the snake tail .
   if (eat===food){
       snake.push(tail);
@@ -75,34 +82,32 @@ function snakeMove() {
 
   snake.unshift(eat);
 
-  $('#cell_'+eat).hasClass('snake-cell'); 
+  $('#cell_'+eat).addClass('snake-cell'); 
     //how to die
-  if (c<0 || r<0 || c>29 || r>19 ||  $('#cell_'+eat).hasClass('snake-cell') ){
+  if (c<0 || r<0 || c>29 || r>19 ){
+      console.log("Dead!");
+      clearInterval(timerId); 
+
       // alert('You lost !');    
-      startGame();
+      // startGame();
       return;
     }  
     $('#cell_'+eat).addClass('snake-cell');       
-    setTimeout(function(){snakeMove()}, speed);
+    // setTimeout(function(){snakeMove()}, speed);
 
 }
 
-// Setup the keyboard 
-// $(document).keydown(function(e) {
-//     console.log(e.keyCode);
-// });
-// I need LEFT = 37  UP = 38  RIGHT =39 DOWN= 40. Possibly space bar to start? = 32
-$(document).keydown(function(e) {
-   if (e.keyCode === 37) {
-    var direction = "left";
-   } else if (e.keyCode === 38) {
-    var direction = "up";
-   } else if (e.keyCode === 39) {
-    var direction = "right";
-   } else if (e.keyCode === 40) {
-    var direction = "down";
-   }
-});
+  $(document).keydown(function(e){
+      if (e.keyCode === 37) { 
+         direction="left";
+      }else if (e.keyCode === 38) { 
+         direction="up";
+      }else if (e.keyCode === 39) { 
+         direction="right";
+      }else if (e.keyCode === 40) { 
+         direction="down";
+    }
+  });
 
 
 }); 
